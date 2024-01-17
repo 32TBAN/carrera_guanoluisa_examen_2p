642ea1eeb9e671a076355d8d62907ff0f3006578
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2024-01-16 20:46:10 -0500

    agergadno eventos de agregar a las tareas personales

---
+++ app/src/main/java/esteban/g/carrera_guanoluisa_examen_2p/AgregarTareaActivityCG.java
@@ -0,0 +1,15 @@
+package esteban.g.carrera_guanoluisa_examen_2p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarTareaActivityCG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_tarea_cg);
+
+    }
+}
--- app/src/main/java/esteban/g/carrera_guanoluisa_examen_2p/ListaTareasCG.java
+++ app/src/main/java/esteban/g/carrera_guanoluisa_examen_2p/ListaTareasCG.java
@@ -1,14 +1,19 @@
 package esteban.g.carrera_guanoluisa_examen_2p;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class ListaTareasCG extends AppCompatActivity {
 
     private LinearLayout personalTasksLayout;
     private LinearLayout businessTasksLayout;
+    private EditText tareaCgEditText;
+    private EditText notaCgEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,15 @@ public class ListaTareasCG extends AppCompatActivity {
 
         personalTasksLayout = findViewById(R.id.personalTasksLayout);
         businessTasksLayout = findViewById(R.id.businessTasksLayout);
-
+        tareaCgEditText = findViewById(R.id.tareaCg);
+        notaCgEditText = findViewById(R.id.notaCG);
+        findViewById(R.id.ivAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListaTareasCG.this, AgregarTareaActivityCG.class);
+                startActivity(intent);
+            }
+        });
         findViewById(R.id.tabPersonal).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
